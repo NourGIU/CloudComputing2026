e370e0b59a90a8baacaa755e2d2aface427485ff
nour_elkholy <nour.elkholy@student.giu-uni.de>
2026-05-21 14:47:15 +0300

    Fix frontend after merge

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,13 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
 
-import App from "./App.jsx";
+import TasksPage from "./pages/TasksPage.jsx";
 import { AuthProvider } from "./auth/AuthContext.jsx";
-import "./styles.css";
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <AuthProvider>
+      <TasksPage />
+    </AuthProvider>
   </React.StrictMode>
 );
